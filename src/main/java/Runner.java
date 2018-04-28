1cf551542d798af21cdb1673a0b80354c7f8a109
Michał Gruchota <gruchot@gmail.com>
2018-04-28 11:25:03 +0200

    eddited Runner, added 3rd line

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello world! - Said Gruchot");
         System.out.println("Hello Gruchot! - Said the world");
+        System.out.println("line 3");
     }
 }
